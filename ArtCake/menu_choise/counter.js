a55216c5ae8fad95846203bfeecdaf3f78c13156
Alexander Fursa <gdm73078@gmail.com>
2023-09-03 22:46:33 +0200

    Add folder

--- ArtCake/menu_choise/counter.js
+++ ArtCake/menu_choise/counter.js
@@ -12,3 +12,21 @@ function decrementCounter() {
     counterInput.value = parseInt(counterInput.value) - 1;
   }
 }
+
+const products = [];
+
+// Получаем элементы кнопки и всплывающего окна
+const buyButton = document.getElementById("buy-button");
+const popup = document.getElementById("popup");
+const closeButton = document.getElementById("close-button");
+
+// При щелчке на кнопке BUE
+buyButton.addEventListener("click", function (e) {
+    e.preventDefault(); // Отменяем стандартное действие ссылки
+    popup.style.display = "block"; // Отображаем всплывающее окно
+});
+
+// При щелчке на кнопке закрытия
+closeButton.addEventListener("click", function () {
+    popup.style.display = "none"; // Скрываем всплывающее окно
+});
