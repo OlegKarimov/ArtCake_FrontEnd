f804cebae74673b44c1dc3c628316e4491f7ab58
Alexander Fursa <gdm73078@gmail.com>
2023-09-04 23:43:21 +0200

    Add folder

--- ArtCake/html-css/artcake.js
+++ ArtCake/html-css/artcake.js
@@ -1,12 +1,12 @@
 
 
 
-// Сценарий для перехода на страницу меню
+//  для перехода на страницу меню
     document.getElementById("Зарегистрируйтесь здесь").addEventListener("click", function() {
       window.location.href = "menu.html";
     });
 
-    // Сценарий для перехода на вторую HTML-страницу
+    // для перехода на вторую HTML-страницу
     document.getElementById("login.html").addEventListener("click", function() {
       window.location.href = "html2.html";
     });
